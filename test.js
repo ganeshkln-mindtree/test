efd23835ccfd0fdbbb0869271bedd097e9754677
ganeshkln-mindtree <ganeshkumar.php@gmail.com>
2015-09-22 14:34:26 +0530

    Update test.js

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ describe('User visits Hello World page', function() {
     });
 
     it('should see Hello World', function() {
-      browser.assert.text('h2', 'Hello World! test test');
+      browser.assert.text('h2', 'Hello World! test test test');
     });
   });
 });
